a5d560312140dd2991eaa7e0675a67937d7d4359
Jordan Dobrev <razpusni.bg@gmail.com>
2018-11-08 14:19:02 +0000

    implement mobile navigation

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -2,16 +2,14 @@
     <nav class="main-nav">
         <div class="nav-links">
             <a
-                href="#Products"
+                href="#News"
                 v-smooth-scroll="{ duration : 2000 }"
             >News</a>
             <a
                 href="#Who-Are-We"
-                v-smooth-scroll="{ duration : 2000 }"
             >Join Us</a>
             <a
                 href="#Contact-Us"
-                v-smooth-scroll="{ duration : 2000 }"
             >Training</a>
         </div>
         <div class="logo">
@@ -27,16 +25,31 @@
         <div class="nav-links justify-end">
             <a
                 href="#Products"
-                v-smooth-scroll="{ duration : 2000 }"
             >How You Can Help</a>
             <a
                 href="#Who-Are-We"
-                v-smooth-scroll="{ duration : 2000 }"
             >Donate</a>
             <a
                 href="#Contact-Us"
-                v-smooth-scroll="{ duration : 2000 }"
             >Contact Us</a>
+        </div>
+
+        <div
+            class="hamburger"
+            @click="visible = !visible"
+        >
+            <div class="w-full"></div>
+            <div class="w-3/4"></div>
+            <div class="w-1/2"></div>
+        </div>
+
+        <div class="mobile-links" v-if="visible" @click="visible = !visible">
+            <a href="#News">News</a>
+            <a href="#Who-Are-We">Join Us</a>
+            <a href="#Contact-Us">Training</a>
+            <a href="#Products">How You Can Help</a>
+            <a href="#Who-Are-We">Donate</a>
+            <a href="#Contact-Us">Contact Us</a>
         </div>
     </nav>
 </template>
@@ -84,7 +97,7 @@ export default {
 }
 
 .nav-links {
-    @apply flex w-1/3;
+    @apply w-1/3 hidden;
 }
 
 .nav-links a {
@@ -93,5 +106,41 @@ export default {
 
 .nav-links a:hover {
     @apply opacity-75;
+}
+
+.mobile-links {
+    @apply fixed pin w-full h-screen flex flex-col items-center justify-center z-40;
+    background: rgba(255, 255, 255, 0.9);
+}
+
+.mobile-links a {
+    @apply mx-2 px-2 my-1 no-underline text-blue-darker text-lg text-xl leading-loose;
+}
+
+.mobile-links a:hover {
+    @apply opacity-75;
+}
+
+.hamburger {
+    @apply cursor-pointer w-8 h-6 flex flex-col items-end justify-between z-50;
+    transition: all 0.25s ease-in-out;
+}
+
+.hamburger > div {
+    @apply h-1 bg-blue-darker rounded-full;
+}
+
+@screen xl {
+    .nav-links {
+        @apply flex;
+    }
+
+    .mobile-links {
+        @apply hidden;
+    }
+
+    .hamburger {
+        @apply hidden;
+    }
 }
 </style>
